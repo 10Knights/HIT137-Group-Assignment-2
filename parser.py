a99a3735c9a27469a7647d0b0e3f37d8e25e4677
cajohns99-a11y <cajohns99@gmail.com>
2026-09-09 21:30:25 +1000

    Correct evaluation logic and error handling in parser
    Fix various errors in the evaluate_tree function and improve error handling.

--- parser.py
+++ parser.py
@@ -102,3 +102,108 @@ def tree_to_string(node):
         return "(neg " + tree_to_string(node[1]) + ")"
     op, left, right = node[1], node[2], node[3]
     return "(" + op + " " + tree_to_string(left) + " " + tree_to_string(right) + ")"
+
+#============================
+# Tree and Error Evaluation
+#============================
+def evaluate_tree(node):
+    # returns (results, error message)
+    if node is None:
+        return None, "Parse Error"
+
+kind = node[0]
+
+#1. number node: ('num';, value)
+if kind == 'num':
+return node [1], None
+
+#2. Unary negation node: ('neg', operand)
+if kind == 'neg':
+    val, err= evaluate_tree(node[1])
+    if err is not None:
+        return none, err 
+    return -val, None
+
+#3. Binary Operation node: ('bin', op, left, right)
+if kind == 'bin':
+    op = node[1]
+    left_node = node[2]
+    right_node = node[3]
+    
+left_val, err = evaluation_tree(left_node)
+if err is not None:
+    return None, err
+
+right_val, err = evaluation_tree(right_node)
+if err is not None:
+    return None, err 
+
+#Performing Calc 
+if op == "+":
+    return left_val + right_val, None
+elif op == '-':
+    return left_val - right_val, None 
+elif op == '*':
+    return left_val * right_val, None 
+elif op == '/':
+    if right_val == 0
+    return None, "division by zero error"
+    return left_val / right_val, None
+elif op == '%':
+    if right_val == 0
+    return non "Modulo by zero error"
+    return left_val % right_val, None
+elif op == '^':
+    return left_val ** right_val, None
+    
+return None, "Uncoded node"
+
+def process_evaluation(node):
+    # Gets the visual tree string from Dantae's functions
+    tree_str = tree_to_string(node)
+
+ig node is None:
+return "ERROR", "ERROR"
+
+result, err = evaluate_tree(node)
+if err is not None:
+    return tree_str, "ERROR"
+
+#formatting to float to integer if its a whole number (eg: 6.0 - 6)
+if ininstance(result, float) and result.is_integer():
+    result=int(result)
+return tree_str, result
+
+    
+    
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
